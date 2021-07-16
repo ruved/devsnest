6f91a711a22711634b985138df2c90e240b47f95
ruved <ruvedalam7776@gmail.com>
2021-07-17 03:50:20 +0530

    tha9 count clickedbox or notclicked

--- web/day9/tha.js
+++ web/day9/tha.js
@@ -12,13 +12,13 @@ for( let i=0;i<box.length;++i)
              box[i].classList.remove('boxclicked');
              box[i].classList.add('box');
              count--;
-             clickcount.innerHTML="<h3>Clicked box="+count+" &nbsp&nbsp&nbsp Unclicked Box="+ (36-count)+"</h3>";
+             clickcount.innerHTML="<h3>Clicked box="+count+" &nbsp&nbsp&nbsp Unclicked Box="+ (box.length-count)+"</h3>";
         }
         else
         {
             box[i].classList.add('boxclicked');
             count++;
-            clickcount.innerHTML="<h3>Clicked box="+count+" &nbsp Unclicked Box="+ (36-count)+"</h3>";
+            clickcount.innerHTML="<h3>Clicked box="+count+" &nbsp Unclicked Box="+ (box.length-count)+"</h3>";
         }  
 });
 }
